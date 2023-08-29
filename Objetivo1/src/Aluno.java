56f1f430416ca1d94858bf2809fb3d2e5b997f57
Gabriel Carvalho <69277918+k1ngabs@users.noreply.github.com>
2023-08-29 01:09:47 -0300

    Initial commit

---
+++ Objetivo1/src/Aluno.java
@@ -0,0 +1,69 @@
+public class Aluno {
+    private int id;
+    private int cpf;
+    private String nome;
+    private String sobrenome;
+    private String email;
+
+    public Aluno() {
+    }
+
+    public Aluno(int id, int cpf, String nome, String sobrenome, String email) {
+        this.id = id;
+        this.cpf = cpf;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+    }
+
+    public Aluno(int cpf, String nome, String sobrenome) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    public Aluno(String nome, String sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
